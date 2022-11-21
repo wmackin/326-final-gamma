cb2ca3894395d481d0f2ac3941838eafe85e85e8
wmackin <wmackin@live.com>
2022-11-20 23:07:11 -0500

    Attempted enabling CORS

--- server.js
+++ server.js
@@ -65,6 +65,13 @@ function dropDownRegions() {
     }
     return options;
 }
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use('/', urlencodedParser, express.static('.'));
 
 app.get('/champion.js', (req, res) => {
