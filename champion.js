8439d3f60b91e1c0675db70534b4bf5d9de81bae
jhernandezum <70250794+jhernandezum@users.noreply.github.com>
2022-12-03 15:19:46 -0500

    Body of post test

--- champion.js
+++ champion.js
@@ -4,10 +4,11 @@ let user = "test"
 sumbitButton.addEventListener('click', async  e => {
     console.log('Event has occured');
     let data = {
-        user : "test",
-        review: document.getElementById("reviewBox").value, 
-        lore: document.getElementById("name").innerText
+        "user" : "test",
+        //review: document.getElementById("reviewBox").value, 
+        //lore: document.getElementById("name").innerText
     };
+    console.log(JSON.stringify(data))
     const response = await fetch('/addPost', {
         method: "POST",
         mode : "no-cors",
