a2384aee17cd9d9e8821e73c70695c2ccfef337a
jhernandezum <70250794+jhernandezum@users.noreply.github.com>
2022-12-03 12:18:35 -0500

    SQL testing

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.post('/addPost', (req, res) => {
     });
 
     client.connect();
-    client.query(`INSERT INTO reviews (username, lore, review, likes) VALUES ('user', 'Graves', 'excellent', 0);`, (err, res) => {
+    client.query(`INSERT INTO reviews (lore, review, likes) VALUES ('Graves', 'excellent');`, (err, res) => {
         if (err) throw err;
         for (let row of res.rows) {
             console.log(JSON.stringify(row));
