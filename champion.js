a61a45fb1dbc02e6133b33d79b273bd4a7f57108
jhernandezum <70250794+jhernandezum@users.noreply.github.com>
2022-12-03 14:40:40 -0500

    Body of post test

--- champion.js
+++ champion.js
@@ -14,26 +14,9 @@ sumbitButton.addEventListener('click', async  e => {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
     }).catch(err => console.log(err));
     console.log('Event Done');
+    location.reload.bind(window.location)
 });
 //sumbitButton.addEventListener('click', location.reload.bind(window.location));
-async function postReview(){
-    console.log('TEST');
-    let data = {
-        user: "test",
-        review: document.getElementById("reviewBox").value, 
-        lore: document.getElementById("name").innerText
-    };
-    await fetch('/addPost', {
-        method: 'POST',
-        mode : 'no-cors',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }).catch(err => console.log(err));
-    console.log('test confirm');
-    return;
-}
