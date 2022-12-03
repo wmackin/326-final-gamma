09b3e9523757ee2383e30a8cfd711c4430f56c4d
jhernandezum <70250794+jhernandezum@users.noreply.github.com>
2022-12-03 13:14:02 -0500

    Modernized event litsener

--- champion.js
+++ champion.js
@@ -1,7 +1,23 @@
 
 const sumbitButton = document.getElementById('sumbitButton');
 let user = "test"
-sumbitButton.addEventListener('click', postReview);
+sumbitButton.addEventListener('click', async  e => {
+    console.log('TEST');
+    let data = {
+        user: "test",
+        review: document.getElementById("reviewBox").value, 
+        lore: document.getElementById("name").innerText
+    };
+    await fetch('/addPost', {
+        method: 'POST',
+        mode : 'no-cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).catch(err => console.log(err));
+    console.log('test confirm');
+});
 sumbitButton.addEventListener('click', location.reload.bind(window.location));
 async function postReview(){
     console.log('TEST');
