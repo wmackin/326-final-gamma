1980e0f8ca6b0c9d6468b3c784d8a5b7ef6e9e56
jhernandezum <70250794+jhernandezum@users.noreply.github.com>
2022-12-03 15:14:12 -0500

    Body of post test

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.post('/addPost', async (req, res) => {
     const queryResult = await client.query(`INSERT INTO reviews (username,lore,review,likes,time_posted) VALUES ('Josh','Graves','Thanks',0,'2022-12-03 19:33:20.663513')`);
     console.log(queryResult);
     client.end();
-    res.send('Success!');
+    res.send(req.body);
 })
 
 app.get('/champion', async (req, res) => {
