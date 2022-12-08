eecafafa6004646f8ab6872ff4b2202f82dd7d34
jhernandezum <70250794+jhernandezum@users.noreply.github.com>
2022-12-08 01:10:12 -0500

    auth

--- signup.js
+++ signup.js
@@ -1,15 +1,6 @@
 const sumbitButton = document.getElementById('signupButton');
 sumbitButton.addEventListener('click', async  e => {
     console.log('Event has occured');
-    console.log({
-        "user" : document.getElementById("username").value,
-        "password": document.getElementById("pwd").value, 
-        "champion": document.getElementById("champion").value,
-        "region": document.getElementById("region").value,
-        "position": document.getElementById("position").value,
-        "story": document.getElementById("story").value,
-        "rank": document.getElementById("rank").value,
-    })
     let data = JSON.stringify({
         "user" : document.getElementById("username").value,
         "password": document.getElementById("pwd").value, 
@@ -19,7 +10,6 @@ sumbitButton.addEventListener('click', async  e => {
         "story": document.getElementById("story").value,
         "rank": document.getElementById("rank").value,
     });
-    console.
     console.log(data)
     const response = await fetch('/signup', {
         method: "POST",
