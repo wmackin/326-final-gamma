6beb1c58fe374298a75269569c43aa2d34ed8241
jhernandezum <70250794+jhernandezum@users.noreply.github.com>
2022-12-08 17:58:29 -0500

    auth

--- server.js
+++ server.js
@@ -164,11 +164,11 @@ function checkLoggedIn(req, res, next) {
     }
 }
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+  });*/
 
 
 
