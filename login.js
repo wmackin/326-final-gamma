ec5ab1d4bdc1b401196cd5887ac46fe841544a35
jhernandezum <70250794+jhernandezum@users.noreply.github.com>
2022-12-08 14:10:48 -0500

    auth

--- login.js
+++ login.js
@@ -1,14 +1,14 @@
-const loginButton = document.getElementById('signupButton');
+const loginButton = document.getElementById('loginButton');
 loginButton.addEventListener('click', async  e => {
     console.log('Event has occured');
-    let data = JSON.stringify({
-        "user" : document.getElementById("username").value,
-        "password": document.getElementById("password").value, 
-    });
+    //let data = JSON.stringify({
+        //"user" : document.getElementById("username").value,
+        //"password": document.getElementById("password").value, 
+    //});
     const response = await fetch('/login', {
         method: "POST",
         headers:  {'Content-Type': 'application/json'},
-        body: data,
+        //body: data,
     })
     console.log(response);
 });
