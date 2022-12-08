0935d03ad3d8626ecaf3adf90a6657e2915e3dc5
jhernandezum <70250794+jhernandezum@users.noreply.github.com>
2022-12-08 01:08:16 -0500

    auth

--- signup.js
+++ signup.js
@@ -2,22 +2,22 @@ const sumbitButton = document.getElementById('signupButton');
 sumbitButton.addEventListener('click', async  e => {
     console.log('Event has occured');
     console.log({
-        "user" : document.getElementById("username").innerText,
+        "user" : document.getElementById("username").value,
         "password": document.getElementById("pwd").value, 
-        "champion": document.getElementById("champion").innerText,
-        "region": document.getElementById("region").innerText,
-        "position": document.getElementById("position").innerText,
-        "story": document.getElementById("story").innerText,
-        "rank": document.getElementById("rank").innerText,
+        "champion": document.getElementById("champion").value,
+        "region": document.getElementById("region").value,
+        "position": document.getElementById("position").value,
+        "story": document.getElementById("story").value,
+        "rank": document.getElementById("rank").value,
     })
     let data = JSON.stringify({
-        "user" : document.getElementById("username").innerText,
+        "user" : document.getElementById("username").value,
         "password": document.getElementById("pwd").value, 
-        "champion": document.getElementById("champion").innerText,
-        "region": document.getElementById("region").innerText,
-        "position": document.getElementById("position").innerText,
-        "story": document.getElementById("story").innerText,
-        "rank": document.getElementById("rank").innerText,
+        "champion": document.getElementById("champion").value,
+        "region": document.getElementById("region").value,
+        "position": document.getElementById("position").value,
+        "story": document.getElementById("story").value,
+        "rank": document.getElementById("rank").value,
     });
     console.
     console.log(data)
